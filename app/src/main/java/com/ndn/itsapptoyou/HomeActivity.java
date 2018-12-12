df13d0010ded90fb676d1e790cc01932cf43c3df
Nicholas Rallakis <nrallakis4@gmail.com>
2018-12-12 14:59:31 +0200

    Commit 2

--- app/src/main/java/com/ndn/itsapptoyou/HomeActivity.java
+++ app/src/main/java/com/ndn/itsapptoyou/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.ndn.itsapptoyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,6 +17,21 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button playButton = findViewById(R.id.btn_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPlayActivity();
+            }
+        });
     }
 
+    private void startPlayActivity() {
+        Intent activity = new Intent(this, PlayActivity.class);
+        startActivity(activity);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    @Override
+    public void onBackPressed() {}
 }
---
+++ app/src/main/java/com/ndn/itsapptoyou/PlayActivity.java
@@ -0,0 +1,19 @@
+package com.ndn.itsapptoyou;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+
+    }
+}

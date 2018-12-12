ad5e1e63e18d180c8cbb99404b1f3487e12b9fb1
Nicholas Rallakis <nrallakis4@gmail.com>
2018-12-12 12:19:24 +0200

    1.5 screens

---
+++ app/src/main/java/com/ndn/itsapptoyou/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.ndn.itsapptoyou;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+}
--- app/src/main/java/com/ndn/itsapptoyou/MainActivity.java
+++ app/src/main/java/com/ndn/itsapptoyou/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ndn.itsapptoyou;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +8,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_Launcher);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
